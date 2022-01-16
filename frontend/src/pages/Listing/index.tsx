62a69a782d205934e0b9a8f8ef2f6566f29a0318
Yuri Jivago <yurijivagoads@gmail.com>
2022-01-16 12:57:34 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -38,9 +38,14 @@ function Listing() {
     };
 
 
+    const handlePageChange = (newPageNumber : number ) => {
+        setPageNumber(newPageNumber);
+    }
+
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
